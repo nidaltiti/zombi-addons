30bf149358af4e27d1892a91b6a0357cee8bd8cf
nidal titi <nidal_1984@hotmail.com>
2024-08-21 12:01:53 +0300

    fix tuktukcima

--- plugin.video.matrix/resources/sites/tuktukcinema.py
+++ plugin.video.matrix/resources/sites/tuktukcinema.py
@@ -2,6 +2,7 @@
 # zombi https://github.com/zombiB/zombi-addons/
 
 import re
+
 	
 from resources.lib.gui.hoster import cHosterGui
 from resources.lib.gui.gui import cGui
@@ -156,6 +157,7 @@ def showPack(sSearch = ''):
             sTitle = aEntry[1].replace("مشاهدة","").replace("مترجمة","").replace("مترجم","").replace("فيلم","").replace("والأخيرة","").replace("مدبلج للعربية","مدبلج").replace("والاخيرة","").replace("كاملة","").replace("حلقات كاملة","").replace("اونلاين","").replace("مباشرة","").replace("انتاج ","").replace("جودة عالية","").replace("كامل","").replace("HD","").replace("السلسلة الوثائقية","").replace("الفيلم الوثائقي","").replace("اون لاين","") 
             sThumb = aEntry[1]
             siteUrl = aEntry[0]
+         
             sDesc = ''
 			
 
@@ -202,7 +204,8 @@ def showMovies(sSearch = ''):
         for aEntry in aResult[1]:
  
             sTitle = aEntry[1].replace("مشاهدة","").replace("مسلسل","").replace("انمي","").replace("مترجمة","").replace("مترجم","").replace("فيلم","").replace("والأخيرة","").replace("مدبلج للعربية","مدبلج").replace("والاخيرة","").replace("كاملة","").replace("حلقات كاملة","").replace("اونلاين","").replace("مباشرة","").replace("انتاج ","").replace("جودة عالية","").replace("كامل","").replace("HD","").replace("السلسلة الوثائقية","").replace("الفيلم الوثائقي","").replace("اون لاين","").replace("الموسم العاشر","S10").replace("الموسم الحادي عشر","S11").replace("الموسم الثاني عشر","S12").replace("الموسم الثالث عشر","S13").replace("الموسم الرابع عشر","S14").replace("الموسم الخامس عشر","S15").replace("الموسم السادس عشر","S16").replace("الموسم السابع عشر","S17").replace("الموسم الثامن عشر","S18").replace("الموسم التاسع عشر","S19").replace("الموسم العشرون","S20").replace("الموسم الحادي و العشرون","S21").replace("الموسم الثاني و العشرون","S22").replace("الموسم الثالث و العشرون","S23").replace("الموسم الرابع والعشرون","S24").replace("الموسم الخامس و العشرون","S25").replace("الموسم السادس والعشرون","S26").replace("الموسم السابع والعشرون","S27").replace("الموسم الثامن والعشرون","S28").replace("الموسم التاسع والعشرون","S29").replace("الموسم الثلاثون","S30").replace("الموسم الحادي و الثلاثون","S31").replace("الموسم الثاني والثلاثون","S32").replace("الموسم الاول","S1").replace("الموسم الثاني","S2").replace("الموسم الثالث","S3").replace("الموسم الثالث","S3").replace("الموسم الرابع","S4").replace("الموسم الخامس","S5").replace("الموسم السادس","S6").replace("الموسم السابع","S7").replace("الموسم الثامن","S8").replace("الموسم التاسع","S9").replace("الموسم","S").replace("S ","S").replace("الحلقة "," E")
-            siteUrl = aEntry[0]
+            siteUrl = aEntry[0] 
+         
             sThumb = aEntry[2]
             sDesc = ''
             sYear = ''
@@ -219,7 +222,7 @@ def showMovies(sSearch = ''):
             oOutputParameterHandler.addParameter('sYear', sYear)
             oOutputParameterHandler.addParameter('sDesc', sDesc)
 			
-            oGui.addMovie(SITE_IDENTIFIER, 'showHosters', sTitle, '', sThumb, sDesc, oOutputParameterHandler)
+            oGui.addMovie(SITE_IDENTIFIER, "showHosters", sTitle, '', sThumb, sDesc, oOutputParameterHandler)
 
         
     if not sSearch:
@@ -423,48 +426,42 @@ def __checkForNextPage(sHtmlContent,sURL):
         if currentPage:
             return sURL.split("?page=")[0] + '?page=' + str(int(currentPage)+1)
     return False
-
 def showHosters():
+   
     oGui = cGui()
-    oParser = cParser()
     oInputParameterHandler = cInputParameterHandler()
     sUrl = oInputParameterHandler.getValue('siteUrl')
+   
     sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
     sThumb = oInputParameterHandler.getValue('sThumb')
 
-    oRequestHandler = cRequestHandler(sUrl)
-    sHtmlContent = oRequestHandler.request()
-
-    oParser = cParser()
-    sPattern = '<a class="watchAndDownlaod" href="([^"]+)' 
-    aResult = oParser.parse(sHtmlContent, sPattern)
-
-    if (aResult[0]):
-        sUrl = aResult[1][0]
-    oRequestHandler = cRequestHandler(sUrl)
+    oRequestHandler = cRequestHandler(sUrl+"/watch/")
     oRequestHandler.addHeaderEntry('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0')
-    oRequestHandler.addHeaderEntry('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7')
+    oRequestHandler.addHeaderEntry('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
     oRequestHandler.addHeaderEntry('X-Requested-With', 'XMLHttpRequest')
     oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
-    oRequestHandler.addHeaderEntry('Accept-Encoding','gzip, deflate, br')
-    oRequestHandler.addHeaderEntry('Referer',sUrl.split('watch/')[0])
     sHtmlContent = oRequestHandler.request()
     # (.+?) ([^<]+) .+?
               
-    sPattern = 'data-link="([^"]+)'
+    sPattern = 'data-link="(.+?)" class='
+    oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
-    VSlog(aResult)
+  
 
     if aResult[0]:
+
         for aEntry in aResult[1]:           
             url = aEntry
+           
             sTitle = " " 
             if url.startswith('//'):
                url = 'http:' + url
 								
             sHosterUrl = url 
+         
             if '?download_' in sHosterUrl:
-                continue
+               sHosterUrl = sHosterUrl.replace("moshahda","ffsff")
+               sHosterUrl = sHosterUrl + "|Referer=" + URL_MAIN 
             if 'userload' in sHosterUrl:
                 sHosterUrl = sHosterUrl + "|Referer=" + URL_MAIN
             if 'moshahda' in sHosterUrl:
@@ -478,7 +475,7 @@ def showHosters():
                 cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumb)
     # (.+?) ([^<]+) .+?
 
-    sPattern = '<a target="_blank" href="([^"]+)".+?class="fa fa-download"></i><span>(.+?)</span>'
+    sPattern = '<a target="_blank" href="(.+?)" class="download--direct"><i class="fa fa-download"></i><span>(.+?)</span>'
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
 
@@ -490,8 +487,10 @@ def showHosters():
                url = 'http:' + url
 								
             sHosterUrl = url 
+          
             if '?download_' in sHosterUrl:
-               continue
+               sHosterUrl = sHosterUrl.replace("moshahda","ffsff")
+               sHosterUrl = sHosterUrl + "|Referer=" + URL_MAIN 
             if 'userload' in sHosterUrl:
                sHosterUrl = sHosterUrl + "|Referer=" + URL_MAIN
             if 'moshahda' in sHosterUrl:
@@ -505,7 +504,7 @@ def showHosters():
                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumb)
     # (.+?) ([^<]+) .+?
 
-    sPattern = '<a target="_NEW" href="([^"]+)'
+    sPattern = '<a target="_NEW" href="(.+?)" class="download--item">'
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
 
@@ -517,8 +516,10 @@ def showHosters():
                url = 'http:' + url
 								
             sHosterUrl = url 
+           
             if '?download_' in sHosterUrl:
-                continue
+              sHosterUrl = sHosterUrl.replace("moshahda","ffsff")
+              sHosterUrl = sHosterUrl + "|Referer=" + URL_MAIN 
             if 'userload' in sHosterUrl:
               sHosterUrl = sHosterUrl + "|Referer=" + URL_MAIN
             if 'moshahda' in sHosterUrl:
