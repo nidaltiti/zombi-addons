c06b875818d48d83a317c6847f40e5e37f74db5d
nidal titi <nidal_1984@hotmail.com>
2025-02-22 12:28:09 +0200

    fix cinmanow

--- plugin.video.matrix/resources/hosters/cimanow.py
+++ plugin.video.matrix/resources/hosters/cimanow.py
@@ -4,11 +4,11 @@ from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.lib.comaddon import dialog, VSlog
 from resources.hosters.hoster import iHoster
-from resources.lib.packer import cPacker
+from resources.lib.util import urlHostName
+from resources.lib import helpers
+from resources.lib import random_ua
 
-
-
-UA = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36 Edg/114.0.1823.67'
+UA = random_ua.get_ua()
 
 class cHoster(iHoster):
 
@@ -16,35 +16,57 @@ class cHoster(iHoster):
         iHoster.__init__(self, 'cimanow', 'CimaNow', 'gold')
 			
     def setUrl(self, sUrl):
-        self._url = str(sUrl).replace('rrsrrs','cimanow')
+        self._url = str(sUrl).replace('rrsrrs','cimanow').replace('rrsrrsn','newcima')
+
+    def _getMediaLinkForGuest(self, autoPlay = False):
         VSlog(self._url)
-    def _getMediaLinkForGuest(self):
-        
+
+        oParser = cParser()
         sReferer = "https://cimanow.cc/"
-        host = self._url.split('/e')[0]
+        sHost = f'https://{urlHostName(self._url)}'
         surl = self._url
 
         oRequest = cRequestHandler(surl)
         oRequest.addHeaderEntry('Referer', sReferer)
         oRequest.addHeaderEntry('User-Agent', UA)
+        oRequest.enableCache(False)
         oRequest.disableSSL()
         sHtmlContent = oRequest.request()
-        oParser = cParser()
-        #VSlog(sHtmlContent)
-       # (.+?) .+? ([^<]+)
 
-        sPattern =  '<source src="(.+?)" type="video/mp4" size="(.+?)">' 
-        aResult = oParser.parse(sHtmlContent,sPattern)
         list_url=[]
         list_q=[]
-        for aEntry in aResult[1]:
-                list_q.append(aEntry[1]) 
-                list_url.append(aEntry[0]) 
-				
-        api_call = dialog().VSselectqual(list_q,list_url)
-        api_call = host + api_call.replace(' ', '%20') +'|AUTH=TLS&verifypeer=false' + '&Referer=' + host
+        sPattern = '<source src="(.+?)" type="video/mp4" size="(.+?)">' 
+        aResult = oParser.parse(sHtmlContent,sPattern)
+        if aResult[0]:
+            for aEntry in aResult[1]:
+                    list_q.append(aEntry[1]) 
+                    list_url.append(aEntry[0]) 
+                    
+            api_call = dialog().VSselectqual(list_q,list_url)
+            api_call = sHost + api_call
+
+        sPattern = (r'\[([0-9]+p)\]\s*([^,]+)"')
+        aResult = oParser.parse(sHtmlContent,sPattern)
+        if aResult[0]:
+            for aEntry in aResult[1]:
+                    list_q.append(aEntry[0]) 
+                    list_url.append(aEntry[1]) 
+                    
+            api_call = dialog().VSselectqual(list_q,list_url)
+            api_call = sHost + api_call.strip()
+
+        headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "identity;q=1, *;q=0",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive",
+            "Host": urlHostName(self._url),
+            "Referer": self._url,
+            "User-Agent": UA,
+            'verifypeer': 'false'
+        }
 
         if api_call:
-                    return True, api_call
+                    return True, api_call.replace(' ', '%20') + helpers.append_headers(headers)
 
         return False, False
